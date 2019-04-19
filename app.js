a425f4e6125eb1cc028830a1ea1d1b55b1e75c45
Drew Palazzari <drewpalazzari@hotmail.com>
2019-04-19 09:47:54 -0600

    See last commit message

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.locals.title = 'Deadpool backend';
+
+module.exports = app;
