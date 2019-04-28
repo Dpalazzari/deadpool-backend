b762135303d313ea0f0d654cc15f09ff265ce3cd
Dalton Wais <georgedaltonwais@gmail.com>
2019-04-28 09:18:43 -0600

    whoops - changing it up a bit

--- tests/seed.test.js
+++ tests/seed.test.js
@@ -1,12 +1,18 @@
-const async = require('async');
+const getData = require('../helpers/mapApiData');
+const url = require('../helpers/consulHelper');
 
 test('pulls data from Marvel API to populate db', async () => {
-  const url = require('../helpers/consulHelper');
+  
   let apiUrl = await url();
   let offSetArray = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400];
-
   let characters = [];
-  let asyncAction = async (offSet, next) => {  
+  let iterateOffSets = async (offsets, next) => {
+    for (let index = 0; index < offsets.length; index++) {
+      await next(offsets[index]);
+    }
+  };
+
+  iterateOffSets(offSetArray, async (offSet) => {
     let options = {
       method: 'GET',
       uri: apiUrl,
@@ -15,19 +21,7 @@ test('pulls data from Marvel API to populate db', async () => {
         offset: offSet
       }
     }
-    let getData = require('../helpers/mapApiData');
-    let apiData = await getData(options)  //doesn't getData return an array of objects?  So characters will end up being an array or arrays of objects rather than an array of objs?
-    characters.push(apiData);  //add data to characters array
-    next(); //callback to tick to the next item in the offSetArray
-  };
-  
-  let onComplete =  err => {
-    //this function fires after all iterations have been complete OR if there is an error
-    err ? console.log(`error in async loop :: ${err}`) : console.log(`async looping completed`);
-  };
-  
-  //  this async package will iterate the first arg, perform the second arg function on each item and then fire the last function after complete
-  //  forEachSeries will hold each iteration until the previous is resolved
-  async.forEachSeries(offSetArray, asyncAction, onComplete);
-
+    let apiData = await getData(options);
+    characters.push(apiData);
+  });
 });
