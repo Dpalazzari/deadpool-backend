803f3a3d39ce67ac1755823660d95eaf8af21ba5
Dalton Wais <georgedaltonwais@gmail.com>
2019-04-28 07:02:29 -0600

    refactoring test with notes

--- tests/seed.test.js
+++ tests/seed.test.js
@@ -1,8 +1,12 @@
+const async = require('async');
+
 test('pulls data from Marvel API to populate db', async () => {
   const url = require('../helpers/consulHelper');
   let apiUrl = await url();
   let offSetArray = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400];
-  const characters = offSetArray.map(async (offSet) => {
+
+  let characters = [];
+  let asyncAction = async (offSet, next) => {  
     let options = {
       method: 'GET',
       uri: apiUrl,
@@ -12,8 +16,18 @@ test('pulls data from Marvel API to populate db', async () => {
       }
     }
     let getData = require('../helpers/mapApiData');
-    let apiData = await getData(options)
-    return apiData
-  })
-  debugger;
-})
+    let apiData = await getData(options)  //doesn't getData return an array of objects?  So characters will end up being an array or arrays of objects rather than an array of objs?
+    characters.push(apiData);  //add data to characters array
+    next(); //callback to tick to the next item in the offSetArray
+  };
+  
+  let onComplete =  err => {
+    //this function fires after all iterations have been complete OR if there is an error
+    err ? console.log(`error in async loop :: ${err}`) : console.log(`async looping completed`);
+  };
+  
+  //  this async package will iterate the first arg, perform the second arg function on each item and then fire the last function after complete
+  //  forEachSeries will hold each iteration until the previous is resolved
+  async.forEachSeries(offSetArray, asyncAction, onComplete);
+
+});
