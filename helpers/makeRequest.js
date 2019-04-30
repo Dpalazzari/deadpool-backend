de942e928a5754e8eed0d445240dd9ff04bfb45f
Drew Palazzari <drewpalazzari@hotmail.com>
2019-04-29 22:08:31 -0600

    Change makeRequest helper to return a promise instead of using promise request library

--- helpers/makeRequest.js
+++ helpers/makeRequest.js
@@ -1,20 +1,17 @@
-module.exports = async (options) => {
-  const request = require('request-promise');
-  const makeRequest = async (request, options) => {
-    try {
-      let response = await request(options);
-      let results = response.data.results.map(element => {
-        let obj = {};
-        obj.name = element.name;
-        obj.description = element.description;
-        obj.infinity_status = 'ALIVE'
-        obj.endgame_status  = 'ALIVE'
-        return obj
+const request = require('request')
+
+module.exports = async (url) => {
+  const pullData = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error){
+          reject(error)
+        } else {
+          resolve(JSON.parse(body))
+        }
       })
-      return results
-    } catch (err) {
-      console.log(err);
-    }
+    })
   }
-  return makeRequest(request, options);
+
+  return pullData(url);
 }
