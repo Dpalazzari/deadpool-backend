5a373fe241dfceffe3a189310b241face25a25a7
Drew Palazzari <drewpalazzari@hotmail.com>
2019-04-29 22:09:14 -0600

    Update test - should return an array of 1491 objects, the total number in API db

--- tests/seed.test.js
+++ tests/seed.test.js
@@ -1,19 +1,26 @@
+const getData = require('../helpers/makeRequest');
+const flatten = require('../helpers/flatten');
+const format  = require('../helpers/format')
+
 test('pulls data from Marvel API to populate db', async () => {
   const url = require('../helpers/consulHelper');
   let apiUrl = await url();
   let offSetArray = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400];
-  const characters = offSetArray.map(async (offSet) => {
-    let options = {
-      method: 'GET',
-      uri: apiUrl,
-      json: true,
-      qs: {
-        offset: offSet
-      }
-    }
-    let getData = require('../helpers/mapApiData');
-    let apiData = await getData(options)
-    return apiData
+  const promises = offSetArray.map(async offSet => {
+    offSetUrl = apiUrl + "&offset=" + offSet
+    let apiData = await getData(offSetUrl)
+    return apiData.data.results
   })
-  debugger;
+  
+  const results = await Promise.all(promises)
+  let flattenedData = flatten(results)
+  // Marvel API has 1491 characters
+  expect(flattenedData.length).toBe(1491)
+
+  // let final = format(flattenedData);
+  // for (let i = 0; i < final.length; i++){
+  //   let keys = Object.keys(final[i])
+  //   expect(keys.length).toBe(4)
+  //   expect(keys.includes('name')).toBe(true)
+  // }
 })
