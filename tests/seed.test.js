55e47898f1a6815f4b4472b8878b78f97156db92
Drew Palazzari <drewpalazzari@hotmail.com>
2019-04-30 20:23:07 -0600

    Update test

--- tests/seed.test.js
+++ tests/seed.test.js
@@ -1,16 +1,16 @@
 const seedHelper = require('../helpers/charactersSeedHelper');
 
-test('pulls data from Marvel API to populate db', async () => {
-  let final = await seedHelper()
+test('pulls data from Marvel API to populate db', () => {
+  seedHelper().then(final => {
+    expect(final.length).toBe(1491)
 
-  expect(final.length).toBe(1491)
-  
-  final.forEach(element => {
-    let keys = Object.keys(element)
-    expect(keys.length).toBe(4)
-    expect(keys.includes('name')).toBe(true)
-    expect(keys.includes('description')).toBe(true)
-    expect(keys.includes('infinity_status')).toBe(true)
-    expect(keys.includes('endgame_status')).toBe(true)
+    final.forEach(element => {
+      let keys = Object.keys(element)
+      expect(keys.length).toBe(6)
+      expect(keys.includes('name')).toBe(true)
+      expect(keys.includes('description')).toBe(true)
+      expect(keys.includes('infinity_status')).toBe(true)
+      expect(keys.includes('endgame_status')).toBe(true)
+    })
   })
 })
