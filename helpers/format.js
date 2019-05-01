490d01b57dbd812bf888cf519e93bee28537f433
Drew Palazzari <drewpalazzari@hotmail.com>
2019-04-30 20:23:01 -0600

    Seed file works for characters. Adds timestamps to the objects.

--- helpers/format.js
+++ helpers/format.js
@@ -2,9 +2,11 @@ module.exports = (arr) => {
   return arr.map(element => {
     let obj = {}
     obj.name = element.name
-    obj.description = element.description
+    obj.description = element.description.substr(0, 254)
     obj.infinity_status = 'ALIVE'
     obj.endgame_status = 'ALIVE'
+    obj.createdAt = new Date()
+    obj.updatedAt = new Date()
     return obj
   })
 }
